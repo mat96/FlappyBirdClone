08a5b21aa2f9c37eac24a011a20efd6edb74e16c
MATTHEW-LAPTOP\Matthew <matthew08221996>
2018-10-31 14:43:06 -0400

    Added Rock pair and is prefabbed. The Rigid body on the rock pair should be kinematic (not yet done)

---
+++ Flappy Bird Clone/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    
+    [SerializeField] Vector2 jumpForce = new Vector2(0, 300);
+
+    Rigidbody2D Rigidbody;
+	// Use this for initialization
+	void Start ()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody.velocity = Vector2.zero;
+            Rigidbody.AddForce(jumpForce);
+        }
+	}
+}
